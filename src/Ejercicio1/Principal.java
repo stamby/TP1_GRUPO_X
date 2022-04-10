36c521f67f95692a0684b09081eb370dc40c7af5
Bruno <stamblonnocorporation@hotmail.com>
2022-04-09 22:55:34 -0300

    Comparar los dos profesores (Ej. 1)

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -2,6 +2,22 @@ package Ejercicio1;
 
 public class Principal {
 	public static void main(String[] args) {
+		Profesor profesor1 = new Profesor();
 		
+		profesor1.setNombre("Marcos Martínez");
+		profesor1.setCargo("Titular");
+		profesor1.setEdad(40);
+		profesor1.setAntiguedadDocente(15);
+		
+		Profesor profesor2 = new Profesor();
+		
+		profesor2.setNombre("Marcos Martínez");
+		profesor2.setCargo("Titular");
+		profesor2.setEdad(40);
+		profesor2.setAntiguedadDocente(15);
+		
+		if (profesor1.esIgualA(profesor2)) {
+			System.out.print("Es el mismo profesor.");
+		}
 	}
 }
